07946f311fc91dfe1e15b684dec64cb9d4a0b7f1
Tikam02 <timonalma81@gmail.com>
2020-12-13 13:23:07 +0530

    kafka experiment

---
+++ busdata2.py
@@ -0,0 +1,48 @@
+#testBusdata
+import os
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+
+
+# Reads coordinates from Geojson
+input_file = open('./data/busdata2.json')
+json_array = json.load(input_file)
+coordinates  = json_array['features'][0]['geometry']['coordinates']
+
+# Generate UUID
+def generate_uuid():
+    return uuid.uuid4()
+
+
+# Kafka Producer
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['testBusdata']
+producer = topic.get_sync_producer()
+
+
+
+# construct messages and send it to kafka
+data = {}
+data['busline'] = '00002'
+
+def generate_checkpoint(coordinates):
+    i = 0
+    while i < len(coordinates):
+        data['key'] = data['busline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coordinates[i][1]
+        data['longitude'] = coordinates[i][0]
+        message = json.dumps(data)
+        print(message)
+        producer.produce(message.encode('ascii'))
+
+        #if the bus reached the destination or last coordinates, it starts from the beggining 
+        if i == len(coordinates)-1:
+            i = 0 
+        else:
+            i = i + 1
+
+generate_checkpoint(coordinates)   
+
